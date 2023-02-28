2666c565caf23bf8d541d3e82750b04ba96b4419
Leonid Medovyy <leonid@storypro.io>
2023-02-28 16:45:12 -0700

    refactoring geocoder into a service, plus basic reliability upgrades

---
+++ app/services/geocoder.rb
@@ -0,0 +1,19 @@
+class Geocoder
+  def initialize
+    @api_str =
+      "https://maps.googleapis.com/maps/api/geocode/json?key=#{ENV['GOOGLE_MAPS_API']}&censor=false&address="
+  end
+
+  def geocode(address: nil, city: nil, state: nil, zip: nil, country: nil)
+    geocoded_address = String.new
+    geocoded_address << "#{address}. "   if address
+    geocoded_address << "#{city}, #{state} "if city && state
+    geocoded_address << zip if zip
+    geocoded_address << " #{country}" if country
+
+    rsp = HTTParty.get("#{@api_str}#{geocoded_address}")
+
+    return false if rsp['results'].blank?
+    coordinates = rsp['results'][0]['geometry']['location']
+  end
+end
