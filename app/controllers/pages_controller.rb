74291bc788c04a9b380ca11560023b11c74f577e
Leonid Medovyy <leonid@storypro.io>
2023-02-28 11:11:45 -0700

    hookee up turbo form and added results page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
+  before_action :search_params, only: :results
   def index
+  end
+
+  def results
+
+  end
+
+  def search_params
+    params.permit(:city)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root "pages#index"
+  get "results", to: "pages#results"
 end
